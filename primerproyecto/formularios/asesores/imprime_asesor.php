f810d72479d3d4c6008357f80084d3fc8aeed3ec
Gabriel Santiago Gaitan Rendon <gabrielgaitanrendon@gmail.com>
2022-06-23 21:58:34 -0500

    mejoras en muestra de datos

--- primerproyecto/formularios/asesores/imprime_asesor.php
+++ primerproyecto/formularios/asesores/imprime_asesor.php
@@ -9,9 +9,13 @@
     <title>Contenido Actual</title>
 </head>
 <body>
-    
-</body>
-</html>
+
+<button type='button' class="btn btn-secondary m-4" onclick= window.location.href='../../index.html'> 
+    <svg id="atras" xmlns="http://www.w3.org/2000/svg" width="150" height="70" fill="currentColor" class="bi bi-skip-backward-btn-fill" viewBox="0 0 16 16">
+        <path d="M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm11.21-6.907L8.5 7.028V5.5a.5.5 0 0 0-.79-.407L5 7.028V5.5a.5.5 0 0 0-1 0v5a.5.5 0 0 0 1 0V8.972l2.71 1.935a.5.5 0 0 0 .79-.407V8.972l2.71 1.935A.5.5 0 0 0 12 10.5v-5a.5.5 0 0 0-.79-.407z"/>
+    </svg>
+</button>
+
 <?php
     include('../../conexion/conexionbasedatos.php');
     $querySelect = "SELECT * FROM asesores";
@@ -35,15 +39,17 @@
                 echo "<td>". $respuesta[4] ."</td>";
                 echo "<td>". $respuesta[5] ."</td>";
                 echo "<td><button type='button' class='btn btn-outline-success' 
-                onclick='window.location.href='edita_asesor.php?idasesor=$respuesta[0]''
-                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' 
-                        class='bi bi-arrow-down-square-fill' viewBox='0 0 16 16'>
+                    onclick= window.location.href='edita_asesor.php?idasesor=$respuesta[0]'>
+                    <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-arrow-down-square-fill' viewBox='0 0 16 16'>
                         <path d='M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z'/>
-                        </svg>
+                    </svg>
                         Edita
-                        </button>
+                </button>
                 </td>";
             echo "</tr>";
         }
     echo "</table>";
 ?>
+</body>
+</html>
+<?php ?>
