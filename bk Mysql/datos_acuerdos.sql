476721d9785f999978b99e75642b8acd89ee7d79
Gabriel Santiago Gaitan Rendón <gabriel.gaitan@repremundo.com.co>
2022-07-22 23:43:18 -0500

    ultimos cambios

--- bk Mysql/datos_acuerdos.sql
+++ bk Mysql/datos_acuerdos.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 19-07-2022 a las 03:08:06
+-- Tiempo de generación: 23-07-2022 a las 04:40:19
 -- Versión del servidor: 5.7.33
 -- Versión de PHP: 7.4.19
 
@@ -28,10 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `acuerdos` (
+  `idacuerdo` int(11) NOT NULL COMMENT 'auto incremento',
   `numobligacion` bigint(20) NOT NULL COMMENT 'numero de obligacion por la cual se realiza acuerdo',
-  `idasesor` int(11) UNSIGNED NOT NULL COMMENT 'Id de asesor encargado de el acuerdo ',
   `idcliente` int(10) UNSIGNED NOT NULL COMMENT 'id de cliente que realiza el acuerdo',
-  `valorcapital` int(11) NOT NULL COMMENT 'valor de el saldo sin descuento',
   `fechaacuerdo` date NOT NULL COMMENT 'fecha en la cual se realiza el acuerdo de pago',
   `fechapago` date NOT NULL COMMENT 'fecha en la cual tendra que pagar',
   `valor` int(11) NOT NULL COMMENT 'valor por el cual se realiza el acuerdo',
@@ -43,9 +42,9 @@ CREATE TABLE `acuerdos` (
 -- Volcado de datos para la tabla `acuerdos`
 --
 
-INSERT INTO `acuerdos` (`numobligacion`, `idasesor`, `idcliente`, `valorcapital`, `fechaacuerdo`, `fechapago`, `valor`, `cuotas`) VALUES
-(318800010056468626, 0, 0, 600000, '2022-02-10', '2022-05-13', 700000, 1),
-(318800010058458922, 0, 0, 488888, '2022-05-10', '2022-05-12', 78888484, 5);
+INSERT INTO `acuerdos` (`idacuerdo`, `numobligacion`, `idcliente`, `fechaacuerdo`, `fechapago`, `valor`, `cuotas`) VALUES
+(1, 318800010056468626, 0, '2022-02-10', '2022-05-13', 700000, 1),
+(2, 318800010058458922, 0, '2022-05-10', '2022-05-12', 78888484, 5);
 
 -- --------------------------------------------------------
 
@@ -75,29 +74,37 @@ INSERT INTO `asesores` (`idasesor`, `nomcompleto`, `fechanacimiento`, `edad`, `c
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `clientes`
+-- Estructura de tabla para la tabla `clientes_deuda`
 --
 
-CREATE TABLE `clientes` (
-  `idcliente` int(11) NOT NULL COMMENT 'numero de identificación de cliente',
+CREATE TABLE `clientes_deuda` (
+  `numobligacion` bigint(20) NOT NULL COMMENT 'numero de obligación a pagar de cliente',
+  `idcliente` varchar(11) COLLATE utf8_unicode_ci NOT NULL COMMENT 'numero de identificacion de cliente con deuda',
   `nombrecliente` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'nombre de el cliente',
   `edad` int(100) NOT NULL COMMENT 'edad de el cliente',
   `ciudadresidencia` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'ciudad de donde reside el cliente',
   `numcelular` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'numero de celular de cliente',
   `correocliente` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'correo de cliente ',
-  `numobligacion` bigint(20) NOT NULL COMMENT 'numero de obligación a pagar de cliente'
+  `saldocapital` int(11) NOT NULL COMMENT 'el 50% de este valor, sera el max a cobrar'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Volcado de datos para la tabla `clientes`
+-- Volcado de datos para la tabla `clientes_deuda`
 --
 
-INSERT INTO `clientes` (`idcliente`, `nombrecliente`, `edad`, `ciudadresidencia`, `numcelular`, `correocliente`, `numobligacion`) VALUES
-(2500045, 'gabriel1', 0, 'Pereira', '3104518752', 'gabrielgaitanrendon@gmail.com', 3000303000),
-(10101010, 'gabrielactualzado', 0, 'Pereira', '3104518752', 'gabrielgaitanrendon@gmail.com', 3000303000),
-(222555522, 'Pruebas 3525', 0, 'ciudad prueba', '3012216779', 'gabrielgaitanrendon@gmail.com', 3099990909),
-(1001116533, 'gabrielactualzado', 0, 'Pereira', '3104518752', 'gabrielgaitanrendon@gmail.com', 3000303000),
-(1254245254, 'sirus', 19, 'Pereira3', '65060480', 'gabrielgaitanrendon@gmail.com', 987498798798);
+INSERT INTO `clientes_deuda` (`numobligacion`, `idcliente`, `nombrecliente`, `edad`, `ciudadresidencia`, `numcelular`, `correocliente`, `saldocapital`) VALUES
+(64354345, '10001116533', 'gabriel gaitan', 3, 'bogota', '3104518752', 'gabrielgaitanrendon@gmail.com', 9000000),
+(9000009990, '10001116533', 'gabriel gaitan', 3, 'bogota', '3104518752', 'gabrielgaitanrendon@gmail.com', 90000000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pagos`
+--
+
+CREATE TABLE `pagos` (
+  `idpago` int(11) NOT NULL COMMENT 'indentificador de pago'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Índices para tablas volcadas
@@ -107,7 +114,7 @@ INSERT INTO `clientes` (`idcliente`, `nombrecliente`, `edad`, `ciudadresidencia`
 -- Indices de la tabla `acuerdos`
 --
 ALTER TABLE `acuerdos`
-  ADD PRIMARY KEY (`numobligacion`);
+  ADD PRIMARY KEY (`idacuerdo`);
 
 --
 -- Indices de la tabla `asesores`
@@ -116,10 +123,26 @@ ALTER TABLE `asesores`
   ADD PRIMARY KEY (`idasesor`);
 
 --
--- Indices de la tabla `clientes`
+-- Indices de la tabla `clientes_deuda`
 --
-ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`idcliente`);
+ALTER TABLE `clientes_deuda`
+  ADD PRIMARY KEY (`numobligacion`);
+
+--
+-- Indices de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  ADD PRIMARY KEY (`idpago`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pagos`
+--
+ALTER TABLE `pagos`
+  MODIFY `idpago` int(11) NOT NULL AUTO_INCREMENT COMMENT 'indentificador de pago';
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
