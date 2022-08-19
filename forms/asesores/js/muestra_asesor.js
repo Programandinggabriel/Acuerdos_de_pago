5fcec1f8cb247fac56fa7610c46344f24d68834a
Gabriel Santiago Gaitan Rendón <gabriel.gaitan@repremundo.com.co>
2022-08-19 14:34:09 -0500

    cambios

---
+++ forms/asesores/js/muestra_asesor.js
@@ -0,0 +1,2 @@
+document.getElementById('retroceso').addEventListener('click', retroceder, false); 
+    function retroceder(){window.location.href='../../../index.html'};
---
+++ forms/clientes/js/editar_cliente.js
@@ -0,0 +1,5 @@
+document.getElementById('retroceso').addEventListener('click', retroceder, false);
+function retroceder(){window.location.href='../../../index.html'};
+
+document.getElementById('eliminar').addEventListener('click', cambiaValor,false);
+function cambiaValor(){document.getElementsByName("accion")[0].value = "eliminar"};
